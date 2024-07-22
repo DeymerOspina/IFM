4a48ffeb010d422b38d1251cd79ae3adcad41e1b
Deymer Ospina <deymer.ospina@gmail.com>
2024-07-22 17:56:11 -0500

    Imagenes y Svg agregados

--- js/cards.js
+++ js/cards.js
@@ -35,12 +35,12 @@ document
 // Overlay Derecho
 
 document.getElementById("overlay").addEventListener("click", function () {
-    var sidePanel = document.getElementById("sidePanel2");
-    var overlay = document.getElementById("overlay");
-    sidePanel.classList.remove("show-animation2");
-    sidePanel.classList.add("hidden");
-    overlay.classList.add("hidden");
-  });
+  var sidePanel = document.getElementById("sidePanel2");
+  var overlay = document.getElementById("overlay");
+  sidePanel.classList.remove("show-animation2");
+  sidePanel.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
 
 // Overlay Izquierdo
 
@@ -51,4 +51,3 @@ document.getElementById("overlay").addEventListener("click", function () {
   sidePanel.classList.add("hidden");
   overlay.classList.add("hidden");
 });
-
