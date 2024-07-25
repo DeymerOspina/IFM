d0ea6826fb3393e72fb86ef12471cf7f7ad18f82
Deymer Ospina <deymer.ospina@gmail.com>
2024-07-24 20:10:11 -0500

    Modificacion en diseño de las paginas y archivos agregados para la prueba de botones y animacion

---
+++ js/cards_service.js
@@ -0,0 +1,26 @@
+// script.js
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.querySelectorAll("button").forEach((button) => {
+//     button.addEventListener("click", function () {
+//       const contentId = "content-service-" + this.id.split("-")[1];
+//       const content = document.getElementById(contentId);
+//       document
+//         .querySelectorAll(".content")
+//         .forEach((c) => c.classList.remove("active"));
+//       content.classList.add("active");
+//     });
+//   });
+// });
+
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".button-container button").forEach((button) => {
+    button.addEventListener("click", function () {
+      const content = this.querySelector(".content");
+      document
+        .querySelectorAll(".content")
+        .forEach((c) => c.classList.remove("active"));
+      content.classList.add("active");
+    });
+  });
+});
