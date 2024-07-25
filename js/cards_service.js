5c213a4a42294c90e289fceeda9ed884e927cdba
Deymer Ospina <deymer.ospina@gmail.com>
2024-07-24 21:48:32 -0500

    Pre-Diseño de los botones y la animacion de los servicios

--- js/cards_service.js
+++ js/cards_service.js
@@ -13,10 +13,24 @@
 // });
 
 // script.js
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.querySelectorAll(".button-container button").forEach((button) => {
+//     button.addEventListener("click", function () {
+//       const content = this.querySelector(".content");
+//       document
+//         .querySelectorAll(".content")
+//         .forEach((c) => c.classList.remove("active"));
+//       content.classList.add("active");
+//     });
+//   });
+// });
+
+// script.js
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".button-container button").forEach((button) => {
+  document.querySelectorAll("button").forEach((button) => {
     button.addEventListener("click", function () {
-      const content = this.querySelector(".content");
+      const contentId = "content-service-" + this.id.split("-")[1];
+      const content = document.getElementById(contentId);
       document
         .querySelectorAll(".content")
         .forEach((c) => c.classList.remove("active"));
