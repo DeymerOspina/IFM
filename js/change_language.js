825a1e0059cd317e7b35d1705b91fa0ee85fd45a
Deymer Ospina <deymer.ospina@gmail.com>
2024-08-05 23:36:47 -0500

    Se cambio el diseño del boton para cambiar idioma en el index

--- js/change_language.js
+++ js/change_language.js
@@ -1,4 +1,5 @@
 // script.js
+
 document.getElementById("languageToggle").addEventListener("click", () => {
   const currentLang = document.documentElement.lang;
   const newLang = currentLang === "es" ? "en" : "es";
@@ -9,8 +10,31 @@ document.getElementById("languageToggle").addEventListener("click", () => {
     element.textContent = element.getAttribute(`data-${newLang}`);
   });
 
-  document.getElementById("languageToggle").textContent =
-    newLang === "es" ? "En / Es" : "Es / En";
-  // document.getElementById("languageToggle").textContent =
-  //   newLang === "es" ? "English" : "Español";
+  const languageToggle = document.getElementById("languageToggle");
+  const languageImage = document.getElementById("languageImage");
+
+  if (newLang === "es") {
+    languageToggle.textContent = " En/Es";
+    languageImage.src = "assets/images/Flag_of_the_United_States.png";
+  } else {
+    languageToggle.textContent = " Es/En";
+    languageImage.src = "assets/svg/Flag_Spain.svg";
+  }
+  // Move the image back to the beginning of the button text
+  languageToggle.insertBefore(languageImage, languageToggle.firstChild);
 });
+
+// document.getElementById("languageToggle").addEventListener("click", () => {
+//   const currentLang = document.documentElement.lang;
+//   const newLang = currentLang === "es" ? "en" : "es";
+//   document.documentElement.lang = newLang;
+
+//   const elements = document.querySelectorAll("[data-en]");
+//   elements.forEach((element) => {
+//     element.textContent = element.getAttribute(`data-${newLang}`);
+//   });
+
+//   document.getElementById("languageToggle").textContent =
+//     newLang === "es" ? "En/Es" : "Es/En";
+
+// });
